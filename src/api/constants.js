847e36acf08d1071e2cf56fe9f9052b9eef980b1
Anand Mishra <anand.mishra@myntra.com>
2021-06-25 13:24:58 +0530

    Deleted files

--- src/api/constants.js
+++
@@ -1,1 +0,0 @@
-export const SEARCH_URL_PATH = 'customsearch/v1';
--- src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit/src/configureStore';
-import searchlistreducer from '../screens/searchlist/searchlistslice';
-import counterReducer from '../screens/Counter/counterSlice';
-export default configureStore({
-  reducer: {
-    searchlist: searchlistreducer,
-    counter: counterReducer,
-  },
-});
