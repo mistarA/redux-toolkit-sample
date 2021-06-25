af57a01d0c52332df42ddc4ad4af1b44f1e87b67
Anand Mishra <anand.mishra@myntra.com>
2021-06-25 13:27:17 +0530

    Updated index

--- index.js
+++ index.js
@@ -6,12 +6,6 @@ import React from 'react';
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-import { Provider } from 'react-redux';
-import store from '../QuerySearchImage/src/app/store';
 
-const AppMain = () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+const AppMain = () => <App />;
 AppRegistry.registerComponent(appName, () => AppMain);
