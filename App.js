a7a6a95c3d677c93671271f53d036a3f435d2eb0
Anand Mishra <anand.mishra@myntra.com>
2021-06-25 13:31:49 +0530

    Updated App

--- App.js
+++ App.js
@@ -6,27 +6,10 @@
  * @flow strict-local
  */
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
-import Counter from './src/screens/Counter/Counter';
-
-import ItemDetailPage from './src/screens/ItemDetailPage';
-import SearchListPage from './src/screens/SearchListPage';
-import SearchPage from './src/screens/SearchPage';
-
+import { Text } from 'react-native';
 const App = () => {
-  const Stack = createStackNavigator();
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {/* <Stack.Screen name="Counter" component={Counter} /> */}
-        <Stack.Screen name="Search Page" component={SearchPage} />
-        <Stack.Screen name="SearchListPage" component={SearchListPage} />
-        <Stack.Screen name="ItemDetailPage" component={ItemDetailPage} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <Text>Hello</Text>;
 };
 
 export default App;
