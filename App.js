852306e2d08c1088a02af6e16eaad6df8f8c9c10
Anand Mishra <anand.mishra@myntra.com>
2021-06-25 13:34:02 +0530

    Updated App

--- App.js
+++ App.js
@@ -1,3 +1,13 @@
+// const BASE_URL = 'https://www.googleapis.com';
+// const API_KEY = 'AIzaSyCSlqYodCt4T9qUmLngac4m3goXBsjAEhw';
+
+// const defaultQueryParams = {
+//   key: API_KEY,
+//   cx: '011476162607576381860:ra4vmliv9ti',
+// };
+
+// export const SEARCH_URL_PATH = 'customsearch/v1';
+
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
